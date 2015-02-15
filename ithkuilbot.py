d8947df9f7f4579606d5b580e2df33a9ee937952
KitN <c.noussis@gmail.com>
2015-02-14 18:28:29 -0800

    Added the main logic. Does not work b/c Unicode.

--- ithkuilbot.py
+++ ithkuilbot.py
@@ -1,6 +1,9 @@
 import praw
 import time
 
+#The strings that will 'summon' the bot:
+summonstring = '!ithkuil'
+
 #What to tell reddit my bot is called.
 user_agent = ("/r/ithkuil Lookup Bot 1.0 by /u/Archare")
 #The reddit object
@@ -18,12 +21,20 @@ with open('shortab.csv') as f:
     for row in reader:
         # row[0] is the abbrev, row[1] is the map.
         abbrev[row[0]] = row[1]
+#The set of all comments already replied to.
+already_done = set()
 
 #The main loop
 while True:
-    submission = r.get_submission(submission_id='2t4ruz')
+    submission = r.get_submission(submission_id='2vxmak')
     flat_comment = praw.helpers.flatten_tree(submission.comments)
     for comment in flat_comment:
-        if comment
-
+        #Finds the string used to invoke the bot.
+        if summonstring in comment.body:
+            #Creates a list of potential 'keys'
+            keys = comment.body.split()
+            for word in keys:
+                if word in abbrev:
+                    comment.reply(abbrev(word))
+            already_done.add(comment.id)
     time.sleep(10)
